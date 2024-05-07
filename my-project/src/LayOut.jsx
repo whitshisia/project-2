3e1b261ad076744da9a4c5a81f0cd308c860c0d6
+ <+>
2024-05-07 15:29:00 +0300

    changes

--- my-project/src/LayOut.jsx
+++ my-project/src/LayOut.jsx
@@ -1,12 +1,8 @@
 import React from 'react'
 import { Link, Outlet } from 'react-router-dom'
-import MyOrders from './pages/MyOrders'
-
-
 function Layout() {
   return (
     <>
-    
 
 <nav class="bg-white border-gray-200 dark:bg-gray-900">
   <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
--- my-project/src/components/Filter.jsx
+++ my-project/src/components/Filter.jsx
@@ -1,9 +1,31 @@
-import React from 'react'
+import React, { Component } from 'react';
 
-function Filter() {
-  return (
-    <div>Filter</div>
-  )
+class Filter extends Component {
+    handleChange = (event) => {
+        this.props.filterChange(event.target.value)
+    }
+
+    render() {
+        return (
+                <select onChange={event =>this.handleChange(event)}>
+                    <option value="All" >All</option>
+                    <option value="Pizza" >Pizza</option>
+                    <option value="Sphagetti">Sphagetti</option>
+                    <option value="Burger">Burger</option>
+                    <option value="Beef">Beef</option>
+                    <option value="Chicken">Chicken</option>
+                    <option value="Fries">Fries</option>
+                    <option value="Pork">Pork</option>
+                    <option value="Lamb">Lamb</option>
+                    <option value="Water">Water</option>
+                    <option value="Juice">Juice</option>
+                    <option value="Coffee">Coffee</option>
+                    <option value="Milkshake">Milkshake</option>
+                    <option value="Soda">Soda</option>
+
+                </select>
+        );
+    }
 }
 
-export default Filter
+export default Filter;
